27cd7ec697fa3ebec0db6c70c5d74562b727ce1e
CaesiumY <mn0658@naver.com>
2018-10-17 00:20:24 +0900

    service-worker cache list added

--- service-worker.js
+++ service-worker.js
@@ -3,21 +3,18 @@ var cacheName = 'weatherPWA-step-6-1';
 var filesToCache = [
   '/',
   '/index.html',
-  '/scripts/app.js',
-  '/styles/inline.css',
-  '/images/clear.png',
-  '/images/cloudy-scattered-showers.png',
-  '/images/cloudy.png',
-  '/images/fog.png',
-  '/images/ic_add_white_24px.svg',
-  '/images/ic_refresh_white_24px.svg',
-  '/images/partly-cloudy.png',
-  '/images/rain.png',
-  '/images/scattered-showers.png',
-  '/images/sleet.png',
-  '/images/snow.png',
-  '/images/thunderstorm.png',
-  '/images/wind.png'
+  '/js/creative.js',
+  '/css/creative.min.css',
+  '/css/creative.css',
+  '/img/header.jpg',
+  '/img/icon.png',
+  '/img/portfolio/thumbnails/1.jpg',
+  '/img/portfolio/thumbnails/2.jpg',
+  '/img/portfolio/thumbnails/3.jpg',
+  '/img/portfolio/thumbnails/4.jpg',
+  '/img/portfolio/thumbnails/5.jpg',
+  '/img/portfolio/thumbnails/6.jpg',
+
 ];
 
 self.addEventListener('install', function(e) {
@@ -43,26 +40,4 @@ self.addEventListener('activate', function(e) {
     })
   );
   return self.clients.claim();
-});
-
-self.addEventListener('fetch', function(e){
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
-  if(e.request.url.indexOf(dataUrl)>-1){
-    e.respondWith(
-      caches.open(dataCacheName).then(function(cache){
-        return fetch(e.request).then(function(response){
-          cache.put(e.request.url, response.clone());
-          return response;
-        });
-      })
-    );
-  }
-  else {
-  e.respondWith(
-    caches.match(e.request).then(function(response){
-      return response || fetch(e.request);
-    })
-  );
- }
 });
