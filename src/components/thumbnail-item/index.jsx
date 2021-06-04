ce95f1bcb4bc5082b8b4d4d38e94869f7a907189
caesiumy <dbs2636@gmail.com>
2021-06-04 23:36:50 +0900

    feat: Add showing post category on thumbnail

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -7,8 +7,22 @@ import './index.scss'
 export const ThumbnailItem = ({ node }) => (
   <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
     <div key={node.fields.slug}>
+      {node.frontmatter.thumbnail && (
+        <div className="image-container">
+          <img
+            src={node.frontmatter.thumbnail.publicURL}
+            alt={node.frontmatter.title || node.fields.slug}
+          />
+        </div>
+      )}
+
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p>{node.frontmatter.date || ''}</p>
+      <p>
+        <span className="thumbnail-category">
+          {node.frontmatter.category || ''}{' '}
+        </span>
+        <span>{node.frontmatter.date || ''}</span>
+      </p>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
     </div>
   </Link>
