9f3877709408426ce8480d06b8ff93325a9ff4b6
caesiumy <dbs2636@gmail.com>
2021-06-04 22:32:27 +0900

    style: Change post preview excerpt length

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,7 @@ export const pageQuery = graphql`
     ) {
       edges {
         node {
-          excerpt(pruneLength: 200, truncate: true)
+          excerpt(pruneLength: 100, truncate: true)
           fields {
             slug
           }
