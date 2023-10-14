0fd6a5b57e7514b862af86ac36514c783e91f02f
caesiumy <dbs2636@gmail.com>
2023-10-14 13:46:14 +0900

    style: Add font pretendard

---
+++ style/fonts/font-pretendard.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const pretendardFont = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  variable: "--font-pretendard",
+});
