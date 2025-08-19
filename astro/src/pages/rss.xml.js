da0df210c12d95933cde52a68a078904d4581cb9
caesiumy <dbs2636@gmail.com>
2025-08-19 19:43:23 +0900

    refactor: consts.ts를 site.ts로 리네이밍
    - rename: consts.ts → site.ts (사이트 관련 상수 의미 명확화)
    - fix: 모든 import 경로를 새 파일명에 맞게 수정
    
    더 명시적인 파일명으로 코드의 의도와 역할을 명확히 표현

--- astro/src/pages/rss.xml.js
+++ astro/src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../shared/constants/consts';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../shared/constants/site';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
