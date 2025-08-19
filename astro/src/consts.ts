2f290a6288b7408b02349f25504f368fe8f1a735
caesiumy <dbs2636@gmail.com>
2025-08-19 18:10:44 +0900

    chore: 사이트 제목을 "Caesiumy Dev Blog"로 변경

--- astro/src/consts.ts
+++ astro/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Caesiumy 블로그';
-export const SITE_DESCRIPTION = '환영합니다! Caesiumy의 개발 블로그입니다.';
+export const SITE_TITLE = "Caesiumy Dev Blog";
+export const SITE_DESCRIPTION = "환영합니다! Caesiumy의 개발 블로그입니다.";
