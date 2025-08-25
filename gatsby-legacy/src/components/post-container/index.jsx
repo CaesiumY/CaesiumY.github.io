271d3cbdf8316649db8c5575d708f8a6e2c194b5
caesiumy <dbs2636@gmail.com>
2025-08-25 09:35:57 +0900

    cleanup: Gatsby 레거시 프로젝트 제거 (마이그레이션 완료)

--- gatsby-legacy/src/components/post-container/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import './index.scss'
-
-export const PostContainer = ({ html }) => (
-  <div className="post-container" dangerouslySetInnerHTML={{ __html: html }} />
-)
