0611c375cb4b66a44cc0a4de3d63610b96706e22
caesiumy <dbs2636@gmail.com>
2025-08-25 09:36:27 +0900

    cleanup: Astro 중간 버전 제거 (AstroPaper로 완전 이전)

--- astro/src/shared/constants/site.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Caesiumy Dev Blog";
-export const SITE_DESCRIPTION = "환영합니다! Caesiumy의 개발 블로그입니다.";
